12c1cac6c201cdcc0666ade4e76c31cf57040279
Allan Monteiro <allan_mlpe@hotmail.com>
2016-08-18 21:34:59 -0300

    Atualização

--- src/HackerRank_Sorting.java
+++ src/HackerRank_Sorting.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class HackerRanking_Sorting {
+public class HackerRank_Sorting {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
