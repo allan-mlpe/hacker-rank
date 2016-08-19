f9700d15277ba7bebfabd86001866c37ec51ec44
Allan Monteiro <allan_mlpe@hotmail.com>
2016-08-18 21:32:25 -0300

    Solução para o problema do dia 25 da seção 30 Days of Code

---
+++ src/HackerRank_NestedLogic.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class HackerRank_NestedLogic {
+	
+	public static boolean isPrime(int n) {
+		boolean r = true;
+		
+		if((n != 2 && n % 2 == 0) || n < 2) {
+			r = false;
+		} 
+		
+		for(int i = 3; i < Math.sqrt(n)+1; i+=2) {
+			if(n%i == 0) {
+				r = false;
+			}
+		}
+				
+		return r;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			int testCases = Integer.parseInt(br.readLine());
+			
+			while(testCases-- > 0) {
+				System.out.println(isPrime(Integer.parseInt(br.readLine())) ? "Prime" : "Not prime");
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
