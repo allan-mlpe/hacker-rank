53a8ba2f49167b0a0337445d6fc659a536b977cc
allan-mlpe <allan_mlpe@hotmail.com>
2018-01-21 09:31:50 -0300

    Solução do problema Java Interface da seção Java->Object Oriented Programming

---
+++ src/HackerRank_JavaInterface.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class HackerRank_JavaInterface {
+
+    interface AdvancedArithmetic {
+        int divisor_sum(int n);
+    }
+
+    static class MyCalculator implements AdvancedArithmetic {
+
+        @Override
+        public int divisor_sum(int n) {
+            return getDivisorSum(n);
+        }
+
+        private int getDivisorSum(int n) {
+
+            if(n == 1) return n;
+
+            int sum = n + 1;
+
+            for(int i = 2; i <= n/2; i++) {
+                if(n % i == 0) sum += i;
+            }
+
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyCalculator my_calculator = new MyCalculator();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        System.out.println(my_calculator.divisor_sum(n));
+        s.close();
+    }
+}
