cff98ce335842de94666c981d2f6846952de0563
allan-mlpe <allan_mlpe@hotmail.com>
2018-01-22 21:47:25 -0300

    Refatoração do problema Java Exception Handling (Try-catch)

--- src/HackerRank_JavaExceptionHandlingTryCatch.java
+++ src/HackerRank_JavaExceptionHandlingTryCatch.java
@@ -4,20 +4,16 @@ import java.util.Scanner;
 public class HackerRank_JavaExceptionHandlingTryCatch {
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        try {
+        try (Scanner s = new Scanner(System.in)) {
             int x = s.nextInt();
             int y = s.nextInt();
 
             int r = x / y;
             System.out.println(r);
-        } catch(ArithmeticException e) {
+        } catch (ArithmeticException e) {
             System.out.println(e.toString());
-        } catch(InputMismatchException e) {
+        } catch (InputMismatchException e) {
             System.out.println(e.getClass().getName());
-        }
-        finally {
-            s.close();
         }
     }
 }
