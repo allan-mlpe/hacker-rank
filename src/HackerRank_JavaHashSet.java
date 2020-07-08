9520c9bcc728d562c658c45124e3ec61fc925a20
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2020-07-07 22:02:26 -0300

    Solução para o problema Java HashSet da seção Java->Data structures

---
+++ src/HackerRank_JavaHashSet.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class HackerRank_JavaHashSet {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+        HashSet<String> pairSet = new HashSet<>();
+        int i = 0;
+
+        while (i < t) {
+            pairSet.add(String.format("%s %s", pair_left[i], pair_right[i]));
+            System.out.println(pairSet.size());
+            i++;
+        }
+    }
+}
